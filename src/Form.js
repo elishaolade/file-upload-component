034e3f8129c3474e73000e24a9e353bc7cb1c7e4
elishaolade <elishaolade@gmail.com>
2021-10-16 09:40:49 -0400

    added tailwind to app

---
+++ src/Form.js
@@ -0,0 +1,11 @@
+import React, {useState,useEffect} from 'react';
+
+const Form = () => {
+    return <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <div className="flex-shrink-0">
+            <div>Hello</div>
+        </div>
+    </div>
+}
+
+export default Form;
